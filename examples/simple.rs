7737cfd0dad1b96e16240298f5156723141099b1
Forb.Jok <forbjok@gmail.com>
2021-05-22 15:17:31 +0200

    chore: Example cleanups

--- examples/simple.rs
+++ examples/simple.rs
@@ -37,13 +37,16 @@ fn update_tiles_system(mut query: Query<&mut TileMap>, mut count: Local<u32>) {
     let upd_tiles = Instant::now();
 
     for mut tilemap in query.iter_mut() {
+        // List to store set tile operations
         let mut tiles: Vec<(IVec2, Option<Tile>)> = Vec::with_capacity((WIDTH * HEIGHT) as usize);
+
         let mut i = *count % 4;
 
         for y in 0..HEIGHT {
             let sprite_index = i % 4;
 
             for x in 0..WIDTH {
+                // Add tile change to list
                 tiles.push((
                     IVec2::new(x, y),
                     Some(Tile {
@@ -56,6 +59,7 @@ fn update_tiles_system(mut query: Query<&mut TileMap>, mut count: Local<u32>) {
             i += 1;
         }
 
+        // Perform tile update
         tilemap.set_tiles(tiles);
     }
 
@@ -63,14 +67,13 @@ fn update_tiles_system(mut query: Query<&mut TileMap>, mut count: Local<u32>) {
 }
 
 fn setup(asset_server: Res<AssetServer>, mut commands: Commands, mut texture_atlases: ResMut<Assets<TextureAtlas>>) {
+    // Load tilesheet texture and make a texture atlas from it
     let texture_handle = asset_server.load("textures/tilesheet.png");
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 4, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
-    let tilemap = TileMap { ..Default::default() };
-
+    // Set up tilemap
     let tilemap_bundle = TileMapBundle {
-        tilemap,
         texture_atlas: texture_atlas_handle.clone(),
         transform: Transform {
             scale: Vec3::splat(1.0),
@@ -80,6 +83,9 @@ fn setup(asset_server: Res<AssetServer>, mut commands: Commands, mut texture_atl
         ..Default::default()
     };
 
+    // Spawn camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn().insert_bundle(tilemap_bundle);
+
+    // Spawn tilemap
+    commands.spawn_bundle(tilemap_bundle);
 }
